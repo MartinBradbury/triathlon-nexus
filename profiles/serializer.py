7c670d310baa1a597fdba8f6b2d47ac6b94dd553
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-07 10:53:54 +0000

    added login/out, authentication and authorisation to edit posts functionality

--- profiles/serializer.py
+++ profiles/serializer.py
@@ -3,9 +3,14 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.first_name')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = ['id', 'owner', 'created_at', 'updated_at', 'first_name', 'last_name',
-                  'email', 'gender', 'fitness_level', 'image', 'content',]
+                  'email', 'gender', 'fitness_level', 'image', 'content', 'is_owner',]
             
