f07eb5efb3351d53e226d1bd472f060e6a07e9f3
Martin Bradbury <martinbradbury@btinternet.com>
2024-06-07 12:40:54 +0000

    added post detail view and added Retrieve, Update and delete features.

--- posts/views.py
+++ posts/views.py
@@ -28,4 +28,43 @@ class PostList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class PostDetail(APIView):
+    """
+    Retrieve a profile or edit it if you own it
+    """
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            post = Posts.objects.get(pk=pk)
+            self.check_object_permissions(self.request, post)
+            return post
+        except Posts.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        post = self.get_object(pk)
+        serializer = PostSerializer(
+            post, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
+
 
